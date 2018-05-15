f99a5ab55bd234999f6bdff19b94ce4b8f481044
millionszhang <zyw1819787473@163.com>
2018-05-15 14:08:18 +0800

    python-spider

--- 知乎/知乎模拟登陆.py
+++ 知乎/知乎模拟登陆.py
@@ -1,151 +1,184 @@
 
 import requests
 import time
-import json
-import os
 import re
-import sys
-import subprocess
-from bs4 import BeautifulSoup as BS
+import base64
+import hmac
+import hashlib
+import json
+import matplotlib.pyplot as plt
+from http import cookiejar
+from PIL import Image
 
-
-class ZhiHuClient(object):
-
-    """连接知乎的工具类，维护一个Session
-    2015.11.11
-
-    用法：
-
-    client = ZhiHuClient()
-
-    # 第一次使用时需要调用此方法登录一次，生成cookie文件
-    # 以后可以跳过这一步
-    client.login("username", "password")
-
-    # 用这个session进行其他网络操作，详见requests库
-    session = client.getSession()
-    """
-
-    # 网址参数是账号类型
-    TYPE_PHONE_NUM = "phone_num"
-    TYPE_EMAIL = "email"
-    loginURL = r"http://www.zhihu.com/login/{0}"
-    homeURL = r"http://www.zhihu.com"
-    captchaURL = r"http://www.zhihu.com/captcha.gif"
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate",
-        "Host": "www.zhihu.com",
-        "Upgrade-Insecure-Requests": "1",
+HEADERS = {
+    'Connection': 'keep-alive',
+    'Host': 'www.zhihu.com',
+    'Referer': 'https://www.zhihu.com/',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'
     }
+LOGIN_URL = 'https://www.zhihu.com/signup'
+LOGIN_API = 'https://www.zhihu.com/api/v3/oauth/sign_in'
+FORM_DATA = {
+    'client_id': 'c3cef7c66a1843f8b3a9e6a1e3160e20',
+    'grant_type': 'password',
+    'source': 'com.zhihu.web',
+    'username': '',
+    'password': '',
+    # 改为'cn'是倒立汉字验证码
+    'lang': 'en',
+    'ref_source': 'homepage'
+}
 
-    captchaFile = os.path.join(sys.path[0], "captcha.gif")
-    cookieFile = os.path.join(sys.path[0], "cookie")
+
+class ZhihuAccount(object):
 
     def __init__(self):
-        os.chdir(sys.path[0])  # 设置脚本所在目录为当前工作目录
+        self.login_url = LOGIN_URL
+        self.login_api = LOGIN_API
+        self.login_data = FORM_DATA.copy()
+        self.session = requests.session()
+        self.session.headers = HEADERS.copy()
+        self.session.cookies = cookiejar.LWPCookieJar(filename='./cookies.txt')
 
-        self.__session = requests.Session()
-        self.__session.headers = self.headers  # 用self调用类变量是防止将来类改名
-        # 若已经有 cookie 则直接登录
-        self.__cookie = self.__loadCookie()
-        if self.__cookie:
-            print("检测到cookie文件，直接使用cookie登录")
-            self.__session.cookies.update(self.__cookie)
-            soup = BS(self.open(r"http://www.zhihu.com/").text, "html.parser")
-            print("已登陆账号： %s" % soup.find("span", class_="name").getText())
+    def login(self, username=None, password=None, load_cookies=True):
+        """
+        模拟登录知乎
+        :param username: 登录手机号
+        :param password: 登录密码
+        :param load_cookies: 是否读取上次保存的 Cookies
+        :return: bool
+        """
+        if load_cookies and self.load_cookies():
+            if self.check_login():
+                return True
+
+        headers = self.session.headers.copy()
+        headers.update({
+            'authorization': 'oauth c3cef7c66a1843f8b3a9e6a1e3160e20',
+            'X-Xsrftoken': self._get_token()
+        })
+        username, password = self._check_user_pass(username, password)
+        self.login_data.update({
+            'username': username,
+            'password': password
+        })
+        timestamp = str(int(time.time()*1000))
+        self.login_data.update({
+            'captcha': self._get_captcha(headers),
+            'timestamp': timestamp,
+            'signature': self._get_signature(timestamp)
+        })
+
+        resp = self.session.post(self.login_api, data=self.login_data, headers=headers)
+        if 'error' in resp.text:
+            print(re.findall(r'"message":"(.+?)"', resp.text)[0])
+        elif self.check_login():
+            return True
+        print('登录失败')
+        return False
+
+    def load_cookies(self):
+        """
+        读取 Cookies 文件加载到 Session
+        :return: bool
+        """
+        try:
+            self.session.cookies.load(ignore_discard=True)
+            return True
+        except FileNotFoundError:
+            return False
+
+    def check_login(self):
+        """
+        检查登录状态，访问登录页面出现跳转则是已登录，
+        如登录成功保存当前 Cookies
+        :return: bool
+        """
+        resp = self.session.get(self.login_url, allow_redirects=False)
+        if resp.status_code == 302:
+            self.session.cookies.save()
+            print('登录成功')
+            return True
+        return False
+
+    def _get_token(self):
+        """
+        从登录页面获取 token
+        :return:
+        """
+        resp = self.session.get(self.login_url)
+        token = re.findall(r'_xsrf=([\w|-]+)', resp.headers.get('Set-Cookie'))[0]
+        return token
+
+    def _get_captcha(self, headers):
+        """
+        请求验证码的 API 接口，无论是否需要验证码都需要请求一次
+        如果需要验证码会返回图片的 base64 编码
+        根据头部 lang 字段匹配验证码，需要人工输入
+        :param headers: 带授权信息的请求头部
+        :return: 验证码的 POST 参数
+        """
+        lang = headers.get('lang', 'en')
+        if lang == 'cn':
+            api = 'https://www.zhihu.com/api/v3/oauth/captcha?lang=cn'
         else:
-            print("没有找到cookie文件，请调用login方法登录一次！")
-
-    # 登录
-    def login(self, username, password):
-        """
-        验证码错误返回：
-        {'errcode': 1991829, 'r': 1, 'data': {'captcha': '请提交正确的验证码 :('}, 'msg': '请提交正确的验证码 :('}
-        登录成功返回：
-        {'r': 0, 'msg': '登陆成功'}
-        """
-        self.__username = username
-        self.__password = password
-        self.__loginURL = self.loginURL.format(self.__getUsernameType())
-        # 随便开个网页，获取登陆所需的_xsrf
-        html = self.open(self.homeURL).text
-        soup = BS(html, "html.parser")
-        _xsrf = soup.find("input", {"name": "_xsrf"})["value"]
-        # 下载验证码图片
-        while True:
-            captcha = self.open(self.captchaURL).content
-            with open(self.captchaFile, "wb") as output:
-                output.write(captcha)
-            # 人眼识别
-            print("=" * 50)
-            print("已打开验证码图片，请识别！")
-            subprocess.call(self.captchaFile, shell=True)
-            captcha = input("请输入验证码：")
-            os.remove(self.captchaFile)
-            # 发送POST请求
-            data = {
-                "_xsrf": _xsrf,
-                "password": self.__password,
-                "remember_me": "true",
-                self.__getUsernameType(): self.__username,
-                "captcha": captcha
-            }
-            res = self.__session.post(self.__loginURL, data=data)
-            print("=" * 50)
-            # print(res.text) # 输出脚本信息，调试用
-            if res.json()["r"] == 0:
-                print("登录成功")
-                self.__saveCookie()
-                break
+            api = 'https://www.zhihu.com/api/v3/oauth/captcha?lang=en'
+        resp = self.session.get(api, headers=headers)
+        show_captcha = re.search(r'true', resp.text)
+        if show_captcha:
+            put_resp = self.session.put(api, headers=headers)
+            img_base64 = re.findall(
+                r'"img_base64":"(.+)"', put_resp.text, re.S)[0].replace(r'\n', '')
+            with open('./captcha.jpg', 'wb') as f:
+                f.write(base64.b64decode(img_base64))
+            img = Image.open('./captcha.jpg')
+            if lang == 'cn':
+                plt.imshow(img)
+                print('点击所有倒立的汉字，按回车提交')
+                points = plt.ginput(7)
+                capt = json.dumps({'img_size': [200, 44],
+                                   'input_points': [[i[0]/2, i[1]/2] for i in points]})
             else:
-                print("登录失败")
-                print("错误信息 --->", res.json()["msg"])
+                img.show()
+                capt = input('请输入图片里的验证码：')
+            # 这里必须先把参数 POST 验证码接口
+            self.session.post(api, data={'input_text': capt}, headers=headers)
+            return capt
+        return ''
 
-    def __getUsernameType(self):
-        """判断用户名类型
-        经测试，网页的判断规则是纯数字为phone_num，其他为email
+    def _get_signature(self, timestamp):
         """
-        if self.__username.isdigit():
-            return self.TYPE_PHONE_NUM
-        return self.TYPE_EMAIL
-
-    def __saveCookie(self):
-        """cookies 序列化到文件
-        即把dict对象转化成字符串保存
+        通过 Hmac 算法计算返回签名
+        实际是几个固定字符串加时间戳
+        :param timestamp: 时间戳
+        :return: 签名
         """
-        with open(self.cookieFile, "w") as output:
-            cookies = self.__session.cookies.get_dict()
-            json.dump(cookies, output)
-            print("=" * 50)
-            print("已在同目录下生成cookie文件：", self.cookieFile)
+        ha = hmac.new(b'd1b964811afb40118a12068ff74a12f4', digestmod=hashlib.sha1)
+        grant_type = self.login_data['grant_type']
+        client_id = self.login_data['client_id']
+        source = self.login_data['source']
+        ha.update(bytes((grant_type + client_id + source + timestamp), 'utf-8'))
+        return ha.hexdigest()
 
-    def __loadCookie(self):
-        """读取cookie文件，返回反序列化后的dict对象，没有则返回None"""
-        if os.path.exists(self.cookieFile):
-            print("=" * 50)
-            with open(self.cookieFile, "r") as f:
-                cookie = json.load(f)
-                return cookie
-        return None
+    def _check_user_pass(self, username, password):
+        """
+        检查用户名和密码是否已输入，若无则手动输入
+        """
+        if username is None:
+            username = self.login_data.get('username')
+            if not username:
+                username = input('请输入手机号：')
+        if '+86' not in username:
+            username = '+86' + username
 
-    def open(self, url, delay=0, timeout=10):
-        """打开网页，返回Response对象"""
-        if delay:
-            time.sleep(delay)
-        return self.__session.get(url, timeout=timeout)
+        if password is None:
+            password = self.login_data.get('password')
+            if not password:
+                password = input('请输入密码：')
+        return username, password
 
-    def getSession(self):
-        return self.__session
 
 if __name__ == '__main__':
-    client = ZhiHuClient()
-
-    # 第一次使用时需要调用此方法登录一次，生成cookie文件
-    # 以后可以跳过这一步
-    # client.login("username", "password")
-
-    # 用这个session进行其他网络操作，详见requests库
-    session = client.getSession()
+    account = ZhihuAccount()
+    account.login(username=None, password=None, load_cookies=True)
